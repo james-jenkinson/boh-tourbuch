d457beb9e11fd112a8c196426bcf5ac484f79232
Emilio Kempkes <emilio.kempkes@akquinet.de>
2023-11-08 11:31:22 +0100

    First faq draft

--- lib/screens/faq/faq_screen.dart
+++ lib/screens/faq/faq_screen.dart
@@ -10,6 +10,7 @@ class FaqScreen extends StatefulWidget {
 
 class _FaqScreenState extends State<FaqScreen> {
   late FaqBloc _faqBloc;
+
   @override
   void initState() {
     _faqBloc = FaqBloc();
@@ -24,13 +25,36 @@ class _FaqScreenState extends State<FaqScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-        child: Center(
+    const List<(String, String)> questions = [
+      ('Warum ist die Banane krumm?', 'Lorem Ipsum'),
+      ('Warum ist die Banane krumm? 2', 'Lorem Ipsum 2'),
+      ('Warum ist die Banane krumm? 3', 'Lorem Ipsum 3')
+    ];
+
+    return SafeArea(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
+              ExpansionPanelList.radio(
+                materialGapSize: 1,
+                children: questions
+                    .asMap()
+                    .entries
+                    .map((q) => ExpansionPanelRadio(
+                        value: q.key,
+                        canTapOnHeader: true,
+                        headerBuilder: (context, isOpen) {
+                          return ListTile(title: Text(q.value.$1));
+                        },
+                        body: ListTile(title: Text(q.value.$2))))
+                    .toList(),
+              )
             ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
